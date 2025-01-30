8fa550c15ebaac7b472bcdd0738c1a358239041e
srinivasabathula07 <134374465+srinivasabathula07@users.noreply.github.com>
2025-01-30 18:41:20 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -5,14 +5,10 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   const today = new Date();
-  //Test code
-  // weekend:
-  // new Date("June 24, 2023 11:13:00");
-  // weekday:
-  // new Date("June 20, 2023 11:13:00");
+  
   const day = today.getDay();
 
-  // console.log(day);
+  
   let type = "a weekday";
   let adv = "it's time to work hard";
 
